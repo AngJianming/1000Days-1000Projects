f6dde863d7fb7966dc4b7922bc821eadaf30fbe8
AngJianming <angjianming2005@hotmail.com>
2024-11-30 20:52:07 +0800

    updates

---
+++ 1-100 Days/Day 82 - Smart Image Carousel/script.js
@@ -0,0 +1,25 @@
+var TrandingSlider = new Swiper('.tranding-slider', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: -40,
+        stretch: 0,
+        depth: 100,
+        modifier: 1.0,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 2000, // Slide changes every 3 seconds
+        disableOnInteraction: false, // Autoplay continues after user interactions
+    },
+});
