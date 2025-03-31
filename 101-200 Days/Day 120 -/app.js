42a27ae2afbd37dd24c51357a97a86e534fc590b
AngJianming <angjianming2005@hotmail.com>
2025-03-31 20:55:37 +0800

    pd

--- 101-200 Days/Day 120 -/app.js
+++ 101-200 Days/Day 120 -/app.js
@@ -67,6 +67,3 @@ console.log(typeof document.body)
 
 console.log(document.body.innerHTML);
 
-console.log(document.body.innerText);
-console.log(document.body.innerText.length);
-console.log(document.body.innerTest[0]);
